c0025e734218b1a47fd1c32fff2a601071ff6987
rnegash <rnegash@macbookproc02w80gehv2g.berlin.internal.babbel.com>
2018-06-19 16:10:58 +0200

    added get quote of the day functionality

--- agora-app/src/App.js
+++ agora-app/src/App.js
@@ -3,10 +3,27 @@ import Quote from "./components/Quote.js";
 import NavBar from "./components/NavBar.js";
 import Response from "./components/Response.js";
 
+import axios from "axios";
+
 import "bulma/css/bulma.css";
 import "./App.css";
 
 class App extends Component {
+  componentWillMount() {
+    this.getDailyQuote();
+  }
+
+  getDailyQuote() {
+    axios
+      .get("http://quotes.rest/qod.json")
+      .then(function(response) {
+        console.log(response.data.contents.quotes[0]);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <section>
