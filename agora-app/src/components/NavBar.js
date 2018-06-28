9104a530974f0c9339c6907698ba6a974e2669e8
rnegash <rnegash@macbookproc02w80gehv2g.berlin.internal.babbel.com>
2018-06-28 14:28:04 +0200

    styling navbar

--- agora-app/src/components/NavBar.js
+++ agora-app/src/components/NavBar.js
@@ -1,24 +1,32 @@
 import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <Link to="/" className="navbar-item">
-          Home
-        </Link>
+        <NavLink to="/" className="navbar-item" activeClassName="is-active">
+          Agora
+        </NavLink>
         {document.cookie.length === 0 ? (
           <Fragment>
-            <Link to="/access" className="navbar-end navbar-item">
+            <NavLink
+              to="/access"
+              className="navbar-end navbar-item"
+              activeClassName="is-active"
+            >
               Login / Register
-            </Link>
+            </NavLink>
           </Fragment>
         ) : (
           <Fragment>
-            <Link to="/myPastResponses" className="navbar-item">
+            <NavLink
+              to="/myPastResponses"
+              className="navbar-item"
+              activeClassName="is-active"
+            >
               My past responses
-            </Link>
+            </NavLink>
             <a
               href="http://localhost:8080/logout"
               className="navbar-end navbar-item"
