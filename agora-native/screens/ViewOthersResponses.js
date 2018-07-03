2afcac7ceabc431ae294f5fedf73b3dc980a73ee
rnegash <rnegash@macbookproc02w80gehv2g.berlin.internal.babbel.com>
2018-07-03 11:58:42 +0200

    list ready on otherpeoplesresponses

--- agora-native/screens/ViewOthersResponses.js
+++ agora-native/screens/ViewOthersResponses.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
-import { Text } from "react-native-paper";
+import { Text, Title, ListItem } from "react-native-paper";
+import { View, KeyboardAvoidingView, StyleSheet, FlatList } from "react-native";
 
 class ViewOthersResponses extends Component {
   static navigationOptions = {
-    title: "View Others Responses"
+    title: "ViewOthersResponses"
   };
 
   render() {
-    return <Text>others list</Text>;
+    return (
+      <View>
+        <FlatList
+          data={[
+            { key: "a" },
+            { key: "b" },
+            { key: "b" },
+            { key: "b" },
+            { key: "a" },
+            { key: "b" },
+            { key: "b" },
+            { key: "b" },
+            { key: "a" },
+            { key: "b" },
+            { key: "b" },
+            { key: "b" }
+          ]}
+          renderItem={({ item }) => (
+            <ListItem title={item.key} description={item.key} />
+          )}
+        />
+      </View>
+    );
   }
 }
 
